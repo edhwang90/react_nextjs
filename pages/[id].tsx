8987649b22b14b88ded3f6b3c8180f371d1504f4
Edy Hwang <42156235+edhwang90@users.noreply.github.com>
2022-06-16 11:33:04 -0400

    indentation
    indentation

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,36 +2,36 @@ import { supabase } from '../utils/supabase';
 import type { Lesson } from '../utils/types';
 
 const LessonDetails = ({lesson}: {lesson: Lesson}) => {
-    return (
-        <div>
-            Lesson details {lesson.id}
-        </div>
-    )  
+  return (
+    <div>
+      Lesson details {lesson.id}
+    </div>
+  )  
 }
 
 export const getStaticPaths = async () => {
-    const { data: lessons } = await supabase.from('lesson').select('id');
+  const { data: lessons } = await supabase.from('lesson').select('id');
 
-    const paths = lessons?.map(({id}: {id: number}) => ({
-        params: {
-            id: id.toString()
-        }
-    }));
-
-    return {
-        paths,
-        fallback: false
+  const paths = lessons?.map(({id}: {id: number}) => ({
+    params: {
+      id: id.toString()
     }
+  }));
+
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export const getStaticProps = async ({params: { id } }) => {
-    const { data: lesson } = await supabase.from('lesson').select('*').eq('id', id).single();
+  const { data: lesson } = await supabase.from('lesson').select('*').eq('id', id).single();
 
-    return {
-        props: {
-            lesson
-        }
+  return {
+    props: {
+      lesson
     }
+  }
 }
 
 export default LessonDetails;
--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,6 @@
 export type Lesson = {
-    id: number,
-    created_at: Date,
-    title: string,
-    description: string
+  id: number,
+  created_at: Date,
+  title: string,
+  description: string
 }
