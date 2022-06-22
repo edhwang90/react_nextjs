5cdf1cbd5880ee08b39c01c0f92fb95f51d9fdbd
Edy Hwang <42156235+edhwang90@users.noreply.github.com>
2022-06-22 15:45:47 -0400

    typescript
    typescript refactor cont'd

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,9 +1,14 @@
 import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 import { supabase } from '../utils/supabase';
 import type { Lesson } from '../utils/types';
 
 interface LessonDetailsProps {
   lesson: Lesson
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string
 }
 
 const LessonDetails: NextPage<LessonDetailsProps> = ({lesson}) => {
@@ -13,7 +18,7 @@ const LessonDetails: NextPage<LessonDetailsProps> = ({lesson}) => {
     </div>
   )  
 }
-// Todo: resolve getStaticPaths: GetStaticPaths
+
 export const getStaticPaths = async () => {
 
   const { data: lessons } = await supabase.from<Lesson>('lesson').select('id');
@@ -23,15 +28,18 @@ export const getStaticPaths = async () => {
       id: id.toString()
     }
   }));
-
+  
   return {
     paths,
     fallback: false
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params: { id } }) => {
-  const { data: lesson } = await supabase.from<Lesson>('lesson').select('*').eq('id', id).single();
+export const getStaticProps: GetStaticProps<LessonDetailsProps, Params> = async (context) => {
+  const { id } = context.params!;
+
+  const res = await supabase.from<Lesson>('lesson').select('*').eq('id', id).single();
+  const lesson = (await res.data) as Lesson;
 
   return {
     props: {
