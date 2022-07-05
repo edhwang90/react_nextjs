753c7925a28104706708a9a2758b6e4d00adbe58
EDH <42156235+edhwang90@users.noreply.github.com>
2022-07-05 09:14:07 -0400

    init Typescript
    init typescript transition

--- pages/api/create-stripe-customer.ts
+++ pages/api/create-stripe-customer.ts
@@ -1,13 +1,16 @@
+import Stripe from 'stripe';
 import initStripe from 'stripe';
 import { supabase } from '../../utils/supabase';
 
-const handler = async (req, res) => {
+import type { StripeCustomer , StripeReq} from '../../utils/types';
+
+const handler = async (req: StripeReq, res) => {
   const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
 
-  const customer = await stripe.customers.create({
+  const customer: StripeCustomer = await stripe.customers.create({
     email: req.body.record.email
   });
-  
+
   await supabase.from('profile').update({
     stripe_customer: customer.id
   }).eq('id', req.body.record.id);
--- utils/types.ts
+++ utils/types.ts
@@ -4,3 +4,29 @@ export type Lesson = {
   title: string,
   description: string
 }
+
+export type StripeCustomer = {
+  id: string,
+  email: string
+}
+
+// type Record = {
+//   id: string,
+//   email: string
+// }
+
+// export type StripeCustomer = {
+//   record: Record
+// }
+
+interface StripeBody {
+  record: StripeCustomer
+}
+
+export interface StripeReq {
+  body: StripeBody
+}
+
+// interface StripeRes {
+//   body: Body
+// }
